1ce9c22c0daf90b33a0840ce2eaf820734867a2b
SHERAN-RANDIKA <randika-se19034@stu.kln.ac.lk>
2023-09-05 16:24:31 +0530

    create paypal

--- my-app/pages/cart.jsx
+++ my-app/pages/cart.jsx
@@ -2,10 +2,91 @@ import React from 'react'
 import styles from '../styles/Cart.module.css'
 import Image from 'next/image';
 import { useDispatch, useSelector } from 'react-redux';
+import {
+    PayPalScriptProvider,
+    PayPalButtons,
+    usePayPalScriptReducer
+} from "@paypal/react-paypal-js";
+
 
 const Cart = () => {
-    const dispatch =useDispatch();
-    const cart = useSelector((state)=>state.cart);
+        const cart = useSelector((state) => state.cart);
+        //const [open, setOpen] = useState(false);
+        //const [cash, setCash] = useState(false);
+        const amount = cart.total;
+        const currency = "USD";
+        const style = { layout: "vertical" };
+        const dispatch = useDispatch();
+     //   const router = useRouter();
+      
+        const createOrder = async (data) => {
+          try {
+            const res = await axios.post("http://localhost:3000/api/orders", data);
+            if (res.status === 201) {
+              dispatch(reset());
+              router.push(`/orders/${res.data._id}`);
+            }
+          } catch (err) {
+            console.log(err);
+          }
+        };
+      
+        // Custom component to wrap the PayPalButtons and handle currency changes
+        const ButtonWrapper = ({ currency, showSpinner }) => {
+          // usePayPalScriptReducer can be use only inside children of PayPalScriptProviders
+          // This is the main reason to wrap the PayPalButtons in a new component
+          const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+      
+        //   useEffect(() => {
+        //     dispatch({
+        //       type: "resetOptions",
+        //       value: {
+        //         ...options,
+        //         currency: currency,
+        //       },
+        //     });
+        //   }, [currency, showSpinner]);
+      
+          return (
+            <>
+              {showSpinner && isPending && <div className="spinner" />}
+              <PayPalButtons
+                style={style}
+                disabled={false}
+                forceReRender={[amount, currency, style]}
+                fundingSource={undefined}
+                createOrder={(data, actions) => {
+                  return actions.order
+                    .create({
+                      purchase_units: [
+                        {
+                          amount: {
+                            currency_code: currency,
+                            value: amount,
+                          },
+                        },
+                      ],
+                    })
+                    .then((orderId) => {
+                      // Your code here after create the order
+                      return orderId;
+                    });
+                }}
+                onApprove={function (data, actions) {
+                  return actions.order.capture().then(function (details) {
+                    const shipping = details.purchase_units[0].shipping;
+                    createOrder({
+                      customer: shipping.name.full_name,
+                      address: shipping.address.address_line_1,
+                      total: cart.total,
+                      method: 1,
+                    });
+                  });
+                }}
+              />
+            </>
+          );
+        };
   return (
     <div className={styles.container}>
         <div className={styles.left}>
@@ -60,6 +141,17 @@ const Cart = () => {
                       <b className={styles.totalTextTitle}>Total:</b>Rs:{cart.total}
                   </div>
                   <button className={styles.btn}>Proceed to checkout</button>
+                  <PayPalScriptProvider
+                options={{
+                  "client-id":
+                    "test",
+                  components: "buttons",
+                  currency: "USD",
+                  "disable-funding": "credit,card,p24",
+                }}
+              >
+                <ButtonWrapper currency={currency} showSpinner={false} />
+              </PayPalScriptProvider>
               </div>
           </div>
     </div>
