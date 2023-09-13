bdcd1caeafb94f7d5db7f9e08a679f72d3ab8251
SHERAN-RANDIKA <randika-se19034@stu.kln.ac.lk>
2023-09-13 23:56:43 +0530

    create admin page delete function

--- my-app/pages/admin/index.jsx
+++ my-app/pages/admin/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import Image from 'next/image'
 import styles from '../../styles/Admin.module.css'
+import axios from 'axios'
 
-const index = () => {
+const index = ({orders,products}) => {
+
+    const handleDelete = async (id) => {
+        try {
+            const res = await axios.delete(`http://localhost:3000/products/` + id);
+            console.log(res)
+        } catch (error) {
+            console.log(error)
+        }
+    }
   return (
     <div className={styles.container}>
         <div className={styles.item}>
@@ -17,26 +27,65 @@ const index = () => {
                         <th className={styles.th}>Actions</th>
                     </tr>
                 </  tbody>
-                <tbody>
+                {products.map((product) => (
+                <tbody key={product._id}>
                     <tr className={styles.trTitle}>
                         <td className={styles.td}>
-                            <Image src = "/images/pizza1.jpg" width={100} height={100} objectFit="cover" />
+                            <Image src = {product.img} width={50} height={50} objectFit="cover" />
                         </td>
-                        <td className={styles.td}>PizzaID</td>
-                        <td className={styles.td}>Pizza Title</td>
-                        <td className={styles.td}>Rs.50</td>
+                        <td className={styles.td}>{product._id.slice(0,5)}...</td>
+                        <td className={styles.td}>{product.title}</td>
+                        <td className={styles.td}>Rs.{product.prices[0]}</td>
                         <td className={styles.td}>
                             <button className={styles.btn}>Edit</button>
-                            <button className={styles.btn}>Delete</button>
+                            <button className={styles.btn} onClick={()=>handleDelete(product._id)}>Delete</button>
+                        </td>
+                    </tr>
+                </  tbody>
+                ))}
+            </table>
+        </div>
+        
+        <div className={styles.item}>
+        <h1 className={styles.title}>Orders</h1>
+            <table className={styles.table}>
+                <tbody>
+                    <tr className={styles.trTitle}>
+                        <th className={styles.th}>ID</th>
+                        <th className={styles.th}>Customer</th>
+                        <th className={styles.th}>Total</th>
+                        <th className={styles.th}>Payment</th>
+                        <th className={styles.th}>Actions</th>
+                    </tr>
+                </  tbody>
+                <tbody>
+                    <tr className={styles.trTitle}>
+                        <td className={styles.td}>{"6578868968997".slice(0,5)}...</td>
+                        <td className={styles.td}>Sheran Randika</td>
+                        <td className={styles.td}>Rs.50</td>
+                        <td className={styles.td}>Paid</td>
+                        <td className={styles.td}>Preparing</td>
+                        <td className={styles.td}>
+                            <button className={styles.btn}>NextStage</button>
                         </td>
                     </tr>
                 </  tbody>
             </table>
-        </div>
-        
-        <div className={styles.item}></div>         
+        </div>         
     </div>
   )
 }
+
+export const getServerSideProps = async () => {
+    const productRes = await axios.get('http://localhost:3000/api/products') 
+    const orderRes = await axios.get('http://localhost:3000/api/orders')
+
+    return {
+        props: {
+            products: productRes.data,
+            orders: orderRes.data
+        }
+    };
+};
 
 export default index
