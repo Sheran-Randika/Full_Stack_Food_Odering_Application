1b70ed6fd044c90fdf83e58274d03c566b6d5322
SHERAN-RANDIKA <randika-se19034@stu.kln.ac.lk>
2023-09-15 22:51:31 +0530

    update admin panel

---
+++ my-app/pages/admin/index.jsx
@@ -0,0 +1,117 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from '../../styles/Admin.module.css'
+import axios from 'axios'
+import { useState } from 'react'
+
+const Index = ({orders,products}) => {
+
+    const [pizzaList, setPizzaList] = useState(products);
+    const [orderList, setorderList] = useState(orders);
+    const status = ["preparing","On the way","delivered"]
+    const handleDelete = async (id) => {
+        try {
+            const res = await axios.delete("http://localhost:3000/api/products/" + id);
+            setPizzaList(pizzaList.filter((pizza) => pizza._id !== id));
+            console.log(res)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleStatus = async (id) => {
+
+        const item = orderList.filter(order => order._id === id)[0];
+        const currentStatus = item.status;
+
+        try {
+            const res = await axios.put("http://localhost:3000/api/orders/" + id, {
+                status : currentStatus + 1 ,
+            });
+            setorderList([ res.data,
+                ...orderList.filter((order) => order._id === id ),
+            ]);
+            // ? res.data : order)));
+            console.log(res)
+        } catch (error) {
+            console.log(err);
+        }
+    };
+  return (
+    <div className={styles.container}>
+        <div className={styles.item}>
+            <h1 className={styles.title}>Products</h1>
+            <table className={styles.table}>
+                <tbody>
+                    <tr className={styles.trTitle}>
+                        <th className={styles.th}>Image</th>
+                        <th className={styles.th}>ID</th>
+                        <th className={styles.th}>Title</th>
+                        <th className={styles.th}>Price</th>
+                        <th className={styles.th}>Actions</th>
+                    </tr>
+                </  tbody>
+                {products.map((product) => (
+                <tbody key={product._id}>
+                    <tr className={styles.trTitle}>
+                        <td className={styles.td}>
+                            <Image src = {product.img} width={50} height={50} objectFit="cover" />
+                        </td>
+                        <td className={styles.td}>{product._id.slice(0,5)}...</td>
+                        <td className={styles.td}>{product.title}</td>
+                        <td className={styles.td}>Rs.{product.prices[0]}</td>
+                        <td className={styles.td}>
+                            <button className={styles.btn}>Edit</button>
+                            <button className={styles.btn} onClick={()=>handleDelete(product._id)}>Delete</button>
+                        </td>
+                    </tr>
+                </  tbody>
+                ))}
+            </table>
+        </div>
+        
+        <div className={styles.item}>
+        <h1 className={styles.title}>Orders</h1>
+            <table className={styles.table}>
+                <tbody>
+                    <tr className={styles.trTitle}>
+                        <th className={styles.th}>ID</th>
+                        <th className={styles.th}>Customer</th>
+                        <th className={styles.th}>Total</th>
+                        <th className={styles.th}>Payment</th>
+                        <th className={styles.th}>Actions</th>
+                    </tr>
+                </  tbody>
+                {orderList.map(order=>(
+                <tbody key={order._id}>
+                    <tr className={styles.trTitle}>
+                        <td className={styles.td}>{order._id.slice(0,5)}...</td>
+                        <td className={styles.td}>{order.customer}</td>
+                        <td className={styles.td}>Rs.{order.total}</td>
+                        <td className={styles.td}>{order.method === 0 ? (<span>cash</span>) : (<span>paid</span>)}</td>
+                        <td className={styles.td}>{status[order.status]}</td>
+                        <td className={styles.td}>
+                            <button className={styles.btn} onClick={()=>handleStatus(order._id)}>NextStage</button>
+                        </td>
+                    </tr>
+                </  tbody>
+                ))}
+            </table>
+        </div>         
+    </div>
+  )
+}
+
+export const getServerSideProps = async () => {
+    const productRes = await axios.get('http://localhost:3000/api/products') 
+    const orderRes = await axios.get('http://localhost:3000/api/orders')
+
+    return {
+        props: {
+            products: productRes.data,
+            orders: orderRes.data
+        }
+    };
+};
+
+export default Index
--- my-app/pages/api/products/[id].js
+++ my-app/pages/api/products/[id].js
@@ -20,22 +20,30 @@ export default async function handler(req, res) {
     }
   }
 
-   if(method === "PUT"){
-     try {
-       const product = await Product.create(req.body);
-       res.status(201).json(product);
-     } catch (err) {
-       res.status(500).json(err);
-     }
-   }
-   
-   if(method === "DELETE"){
-     try {
-       const product = await Product.create(req.body);
-       res.status(201).json(product);
-     } catch (err) {
-       res.status(500).json(err);
-     }
-   }
+  if (method === "PUT") {
+    if(!token || token !== process.env.token){
+      return res.status(401).json("Not authenticated!")
+    }
+    try {
+      const product = await Product.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      res.status(200).json(product);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
-  
+
+  if (method === "DELETE") {
+    // if(!token || token !== process.env.token){
+    //   return res.status(401).json("Not authenticated!")
+   
+    // }
+    try {
+      await Product.findByIdAndDelete(id);
+      res.status(200).json("The product has been deleted!");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+}
