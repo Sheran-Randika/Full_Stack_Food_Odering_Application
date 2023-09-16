2328c3cfa644ce078341f561917518f730d18b2c
SHERAN-RANDIKA <randika-se19034@stu.kln.ac.lk>
2023-09-16 20:19:38 +0530

    fix login errors

--- my-app/pages/admin/login.jsx
+++ my-app/pages/admin/login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
 
     const handleClick = async () => {
         try{
-            await axios.post("http://localhost:3000/admin/login", {
+            await axios.post("http://localhost:3000/api/login", {
                 username,
                 password,
             });
